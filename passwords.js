c9667c78da6cb630a9a65e64065119c798d708cf
ZakariyaAhmed25 <123665141+ZakariyaAhmed25@users.noreply.github.com>
2023-04-10 00:09:45 -0500

    Update passwords.js

--- passwords.js
+++ passwords.js
@@ -3,9 +3,9 @@ function validate() {
     var Password2 = document.getElementById("Password2").value;
 
     if (Password1.length < 8) {
-        alert("Password 1 must be atleast 8 letters")
+        alert("Password 1 must be atleast 8 letters long")
     } else if (Password2.length < 8) {
-        alert("Password 2 must be atleast 8 letters")
+        alert("Password 2 must be at least 8 letters long")
     } else if (Password1 != password2) {
         alert("Passwords don't match")
     } else {
